e153aae5edca54b035886b74d230f1a0702f880e
LinlyBoi <libkyy@e.email>
2023-12-18 00:22:03 +0200

    debug added idky

--- src/arrivals.rs
+++ src/arrivals.rs
@@ -6,7 +6,7 @@ pub fn init_arrivals_scope() -> actix_web::Scope {
 }
 use chrono::NaiveTime;
 use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Arrival {
     a_id: i32,
     time_of_day: NaiveTime,
@@ -16,12 +16,13 @@ pub struct Arrival {
 }
 use web::Data;
 use sqlx::{PgPool, query_as};
-#[get("arrivals")]
+#[get("all")]
 async fn show_arrivals(db_pool: Data<PgPool>) -> impl Responder {
     let arrivals = query_as!(Arrival, r#"SELECT * FROM arrivals"#)
         .fetch_all(db_pool.get_ref())
         .await
         .expect("Could not fetch arrivals");
+	dbg!(&arrivals);
 	HttpResponse::Ok()
         .content_type("application/json")
         .json(arrivals)
