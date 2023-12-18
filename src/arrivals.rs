b011f3f972f76ae3176c1298ad1b65dc5077d058
Supermjork <markehabaziz@gmail.com>
2023-12-18 18:18:52 +0200

    FOUND DYNAMICALLY BUILDING SQLX QUERIES TOO LATE

--- src/arrivals.rs
+++ src/arrivals.rs
@@ -15,13 +15,35 @@ pub struct Arrival {
     direction: bool,
 }
 use web::Data;
-use sqlx::{PgPool, query_as};
+use sqlx::{PgPool, query_as, query};
 #[get("all")]
 async fn show_arrivals(db_pool: Data<PgPool>) -> impl Responder {
     let arrivals = query_as!(Arrival, r#"SELECT * FROM arrivals"#)
         .fetch_all(db_pool.get_ref())
         .await
         .expect("Could not fetch arrivals");
+	dbg!(&arrivals);
+	HttpResponse::Ok()
+        .content_type("application/json")
+        .json(arrivals)
+}
+
+#[get("specific")]
+async fn show_specific(db_pool: Data<PgPool>, type_arr: Vec<u8>) -> impl Responder {
+    // Query Logic
+    // Idea: Construct a query bit by bit depending on input
+
+    let mut dyn_query = String::from("SELECT  * FROM arrivals WHERE ");
+
+    if type_arr.len() == 1 {
+        dyn_query.push_str(&format!("tram_line = {}", type_arr[0]));
+    }
+
+
+    // Check: https://stackoverflow.com/questions/74956100/how-to-build-safe-dynamic-query-with-sqlx-in-rust
+    // lappy ded ;-;
+    let arrivals = String::from("AIDS");
+
 	dbg!(&arrivals);
 	HttpResponse::Ok()
         .content_type("application/json")
