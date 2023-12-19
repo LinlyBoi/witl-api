1a49b551b3f77cd70cbae1b8df5eabfa686afe2f
linlyboi <linlysmolworthy@gmail.com>
2023-12-19 12:30:47 +0200

    FORMAT

--- src/arrivals.rs
+++ src/arrivals.rs
@@ -1,7 +1,9 @@
 use actix_web::{get, web, HttpResponse, Responder};
 
 pub fn init_arrivals_scope() -> actix_web::Scope {
-    let scope = web::scope("/arrivals").service(show_arrivals);
+    let scope = web::scope("/arrivals")
+        .service(show_arrivals)
+        .service(show_specific);
     scope
 }
 use chrono::NaiveTime;
@@ -15,16 +17,16 @@ pub struct Arrival {
     direction: bool,
 }
 
+use sqlx::{query_as, Execute, PgPool, QueryBuilder};
 use web::Data;
-use sqlx::{PgPool, query_as, QueryBuilder, Execute};
 #[get("all")]
 async fn show_arrivals(db_pool: Data<PgPool>) -> impl Responder {
     let arrivals = query_as!(Arrival, r#"SELECT * FROM arrivals"#)
         .fetch_all(db_pool.get_ref())
         .await
         .expect("Could not fetch arrivals");
-	dbg!(&arrivals);
-	HttpResponse::Ok()
+    dbg!(&arrivals);
+    HttpResponse::Ok()
         .content_type("application/json")
         .json(arrivals)
 }
@@ -35,19 +37,22 @@ struct ArrivalFilter {
 }
 
 #[get("specific")]
-async fn show_specific(db_pool: Data<PgPool>, filter: web::Query<ArrivalFilter> ) -> impl Responder {
-
+async fn show_specific(db_pool: Data<PgPool>, filter: web::Query<ArrivalFilter>) -> impl Responder {
     //Le query
-    let arrivals = query_as!(Arrival, "SELECT * FROM arrivals WHERE tram_line = $1 AND week_day = $2", filter.tram_line, filter.week_day)
-        .fetch_all(db_pool.get_ref())
-        .await
-        .expect("Could not fetch arrivals");
-
+    let arrivals = query_as!(
+        Arrival,
+        "SELECT * FROM arrivals WHERE tram_line = $1 AND week_day = $2",
+        filter.tram_line,
+        filter.week_day
+    )
+    .fetch_all(db_pool.get_ref())
+    .await
+    .expect("Could not fetch arrivals");
 
     // Delet cuz we hardcoding
 
-	// dbg!(&arrivals);
-	HttpResponse::Ok()
+    // dbg!(&arrivals);
+    HttpResponse::Ok()
         .content_type("application/json")
         .json(arrivals)
 }
